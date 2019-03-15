ea0d813b5beb4a436f313e8f36162ce598748ffa
Rasmus Odgaard <r.odgaard@gmail.com>
2019-03-15 12:41:08 +0000

    Edgecollider-maskinen

---
+++ Assets/Rasmus/EdgeColliderScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EdgeCollider2D))]
+public class EdgeColliderScript : MonoBehaviour
+{
+
+    EdgeCollider2D collider;
+
+    void Start()
+    {
+        collider = GetComponent<EdgeCollider2D>();
+        collider.points = new Vector2[] {   new Vector2(-0.5f   ,  0.5f), 
+                                            new Vector2( 0.5f   ,  0.5f), 
+                                            new Vector2( 0.5f   , -0.5f), 
+                                            new Vector2(-0.5f   , -0.5f), 
+                                            new Vector2(-0.5f   ,  0.5f) };
+    }
+
+    void Update()
+    {
+        
+    }
+}

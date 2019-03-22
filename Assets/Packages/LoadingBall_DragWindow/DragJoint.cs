25132c2383a052b78030cfea1f0580c4532ca733
jacob hvid amstrup <jacobhvidamstrup@gmail.com>
2019-03-22 09:46:26 +0000

    loading ball og drag window

---
+++ Assets/Packages/LoadingBall_DragWindow/DragJoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragJoint : MonoBehaviour
+{
+
+    public GameObject j;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Vector3 mpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        j.transform.position = new Vector3(mpos.x, mpos.y, 0);
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            gameObject.GetComponent<HingeJoint>().breakForce = 0;
+            gameObject.GetComponent<HingeJoint>().breakTorque = 0;
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0.0000001f, 0, 0));
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            HingeJoint hj = gameObject.AddComponent<HingeJoint>() as HingeJoint;
+            gameObject.GetComponent<HingeJoint>().connectedBody = j.GetComponent<Rigidbody>();
+        }
+    }
+}

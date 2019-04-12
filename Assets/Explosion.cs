70f165f6ecfdffe6b70f2723a06a23a7c764fbe3
jacob hvid amstrup <jacobhvidamstrup@gmail.com>
2019-04-12 07:25:23 +0000

    Explosion script

--- Assets/Explosion.cs
+++ Assets/Explosion.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
-
+    public float timer = 0;
     public Collider[] hitColliders;
 
     void Start()
@@ -13,6 +13,22 @@ public class Explosion : MonoBehaviour
 
     void Update()
     {
-        hitColliders = Physics.OverlapSphere(transform.position, 10);
+        if(timer >= 1){
+            hitColliders = Physics.OverlapSphere(transform.position, 10);
+            for (int i = 0; i < hitColliders.Length; i++){
+                if(hitColliders[i].tag == "Icon"){
+                    hitColliders[i].GetComponent<Rigidbody>().AddForce(new Vector3(0, 25, 0), ForceMode.Impulse);
+                }
+            }
+
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if(Input.GetMouseButton(0)){
+            timer += Time.deltaTime;
+        }
     }
 }

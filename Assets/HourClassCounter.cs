89c03a27788a0a607806584a0f4dc1e2e0030fcd
Nikolaj Schildt Christensen <schildt.nikolaj@gmail.com>
2019-05-03 11:54:49 +0000

    Hourglass with loading bar + prefabs

---
+++ Assets/HourClassCounter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HourClassCounter : MonoBehaviour
+{
+
+    public float loadingScore;
+    public LoadingBar LB;
+    void Start()
+    {
+        LB = FindObjectOfType<LoadingBar>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Sand")
+        {
+            loadingScore += 0.002f;
+            LB.Grow();
+
+        }
+    }
+
+
+        
+
+}
---
+++ Assets/LoadingBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingBar : MonoBehaviour
+{
+
+    public HourClassCounter HGC;
+
+    void Start()
+    {
+        HGC = FindObjectOfType<HourClassCounter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Grow()
+    {
+
+        transform.localScale += new Vector3(HGC.loadingScore, 0f, 0f);
+
+
+    }
+
+}

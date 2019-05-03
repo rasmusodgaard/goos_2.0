b22e5b1e1d275e86d15c3684148635238550f623
Rasmus Odgaard <r.odgaard@gmail.com>
2019-05-03 10:46:07 +0000

    Username print

---
+++ Assets/UsernameScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UsernameScript : MonoBehaviour
+{
+
+    public Text text;
+
+    public List<GameObject> letters;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        letters.Add(other.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        letters.Remove(other.gameObject);
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            foreach (GameObject letter in letters)
+            {
+                print(letter.GetComponentInChildren<Text>().text);
+            }
+        }
+    }
+}

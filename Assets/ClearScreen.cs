825937e8265df2d318f396d82824c96695d78f1e
Nikolaj Schildt Christensen <schildt.nikolaj@gmail.com>
2019-05-03 09:38:28 +0000

    login screen donejonbonjovi

---
+++ Assets/ClearScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearScreen : MonoBehaviour
+{
+
+    public bool moving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            moving = true;
+            print("SPace pressed");
+        }
+
+        if (moving)
+        {
+            gameObject.transform.Translate(Vector3.down * Time.deltaTime);
+
+        }
+    }
+
+ 
+
+}

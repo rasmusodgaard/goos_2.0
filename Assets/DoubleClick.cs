35704c77aadbae7b6b499b724e13a43aa786a9f1
jacob hvid amstrup <jacobhvidamstrup@gmail.com>
2019-05-10 12:08:08 +0000

    New hourglass, quit icon

--- Assets/DoubleClick.cs
+++ Assets/DoubleClick.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DoubleClick : MonoBehaviour
 {
     public GameObject[] windowPrefab;
+    public bool offButton = false;
 
     private int currentSprite;
 
@@ -28,7 +29,11 @@ public class DoubleClick : MonoBehaviour
                 clicked = 0;
                 clicktime = 0;
                 Debug.Log(gameObject.name + " DOUBLE CLICKED");
-                InstantiateWindow(currentSprite); 
+                if(offButton == false) {
+                    InstantiateWindow(currentSprite);
+                } else { 
+                    //to game manager coroutine
+                }
             }
             else if (clicked > 2 || Time.time - clicktime > 1) clicked = 0;
         }
--- Assets/IconSpriteSelector.cs
+++ Assets/IconSpriteSelector.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class IconSpriteSelector : MonoBehaviour
 {
-    public enum SpriteSelector {clock,calculator,music,mail};
+    public enum SpriteSelector {clock,calculator,music,mail,off};
     public SpriteSelector CurrentSprite;
 
     [Header("Sprite Arrays")]

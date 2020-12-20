c113b8cb896c8acb5699e80976194c19f692c607
rasmusodgaard <r.odgaard@gmail.com>
2020-12-20 22:23:00 +0100

    added loading bar with animation

--- Assets/LoadingAnimation_Script.cs
+++ Assets/LoadingAnimation_Script.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using DG.Tweening;
 
 
 public class LoadingAnimation_Script : MonoBehaviour
 {
     TextMeshProUGUI text;
+    Public Ease ease;
 
     void Start()
     {
         text = GetComponent<TextMeshProUGUI>();
-        text.text = "Lol";
+        text.DOText("Loading...",5).SetEase(ease).SetLoops(-1);
     }
+
+
 }

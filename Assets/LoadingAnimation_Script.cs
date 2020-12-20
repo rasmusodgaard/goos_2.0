e3b5f6f97fae64a8843343e9da17d51f86822d28
rasmusodgaard <r.odgaard@gmail.com>
2020-12-20 19:19:46 +0100

    Added font and Loading txt, still needs animation

---
+++ Assets/LoadingAnimation_Script.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class LoadingAnimation_Script : MonoBehaviour
+{
+    TextMeshProUGUI text;
+
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        text.text = "Lol";
+    }
+}
